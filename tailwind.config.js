e340f680e47e2a050443d88f0914a0014dbf8cdf
mortomulu <mfajrihidayat09@gmail.com>
2024-02-25 15:54:12 +0700

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'roboto': ['Roboto', 'sans-serif'],
+        'poppins' : ['poppins']
+      }
+    },
   },
   plugins: [],
 }
